e0bb9b061d73b81243c51cde398d6284cba331c6
Elizabeth <tjeudy.e@gmail.com>
2023-03-07 15:19:00 -0500

    created board component
    added rows which pull from the letter component with letting positioning and values passing through

---
+++ src/components/Board.js
@@ -0,0 +1,61 @@
+import react, { useState } from "react";
+import "./Board.css";
+import { boardDefault } from "./Words";
+import Letters from "./Letters";
+
+export default function Board() {
+	return (
+		<div className="board">
+			<div className="row">
+				{" "}
+				<Letters letterPosition={0} attemptValue={0} />
+				<Letters letterPosition={1} attemptValue={0} />
+				<Letters letterPosition={2} attemptValue={0} />
+				<Letters letterPosition={3} attemptValue={0} />
+				<Letters letterPosition={4} attemptValue={0} />
+				<Letters letterPosition={5} attemptValue={0} />
+			</div>
+			<div className="row">
+				<Letters letterPosition={0} attemptValue={1} />
+				<Letters letterPosition={1} attemptValue={1} />
+				<Letters letterPosition={2} attemptValue={1} />
+				<Letters letterPosition={3} attemptValue={1} />
+				<Letters letterPosition={4} attemptValue={1} />
+				<Letters letterPosition={5} attemptValue={1} />
+			</div>
+			<div className="row">
+				<Letters letterPosition={0} attemptValue={2} />
+				<Letters letterPosition={1} attemptValue={2} />
+				<Letters letterPosition={2} attemptValue={2} />
+				<Letters letterPosition={3} attemptValue={2} />
+				<Letters letterPosition={4} attemptValue={2} />
+				<Letters letterPosition={5} attemptValue={2} />
+			</div>
+			<div className="row">
+				<Letters letterPosition={0} attemptValue={3} />
+				<Letters letterPosition={1} attemptValue={3} />
+				<Letters letterPosition={2} attemptValue={3} />
+				<Letters letterPosition={3} attemptValue={3} />
+				<Letters letterPosition={4} attemptValue={3} />
+				<Letters letterPosition={5} attemptValue={3} />
+			</div>
+			<div className="row">
+				<Letters letterPosition={0} attemptValue={4} />
+				<Letters letterPosition={1} attemptValue={4} />
+				<Letters letterPosition={2} attemptValue={4} />
+				<Letters letterPosition={3} attemptValue={4} />
+				<Letters letterPosition={4} attemptValue={4} />
+				<Letters letterPosition={5} attemptValue={4} />
+			</div>
+			<div className="row">
+				<Letters letterPosition={0} attemptValue={5} />
+				<Letters letterPosition={1} attemptValue={5} />
+				<Letters letterPosition={2} attemptValue={5} />
+				<Letters letterPosition={3} attemptValue={5} />
+				<Letters letterPosition={4} attemptValue={5} />
+				<Letters letterPosition={5} attemptValue={5} />
+			</div>
+		</div>
+	);
+}
+// attempt values represent each box letter attempt at each row
