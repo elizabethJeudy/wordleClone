a201c9170da429981d4ee97932534e12cdaf90c5
Elizabeth <tjeudy.e@gmail.com>
2023-03-07 15:19:31 -0500

    import createContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { createContext } from "react";
+
+export const AppContext = createContext();
 
 export default function App() {
+	const [board, setBoard] = useState(boardDefault);
 	return (
 		<div className="App">
 			<div className="container">
 				<Nav />
+				{/* gives access to pass the states with value */}
+				<AppContext.Provider value={{ board, setBoard }}>
+					<Board />
+					<Keyboard />
+				</AppContext.Provider>
 			</div>
 		</div>
 	);
