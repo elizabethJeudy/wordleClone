c4247caa32972b6d21b81faedaaa78b35ef0c3d6
Elizabeth <tjeudy.e@gmail.com>
2023-03-08 17:54:44 -0500

    added useEffect to track keys
    added function which listens for keys user types adding them on the board; user is able to enter and delete keys from keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import "./Keyboard.css";
 import Key from "./Key";
+
 import { RiDeleteBack2Line } from "react-icons/ri";
+import { AppContext } from "../App";
 
 export default function Keyboard() {
+	const { onSelectLetter, onEnter, onDelete } = useContext(AppContext);
 	// keys for each line
 	const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 	const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 	const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+	// useEffect will call a specific function which will
+	// handle the events on keyboard;
+	// event will render typed key on keyboard
+	const handleKeyboard = useCallback((event) => {
+		if (event.key === "Enter") {
+			onEnter();
+		} else if (event.key === "Backspace") {
+			onDelete();
+		} else {
+			keys1.forEach((key) => {
+				if (event.key.toLowerCase() === key.toLowerCase()) {
+					onSelectLetter(key);
+				}
+			});
+			keys2.forEach((key) => {
+				if (event.key.toLowerCase() === key.toLowerCase()) {
+					onSelectLetter(key);
+				}
+			});
+			keys3.forEach((key) => {
+				if (event.key.toLowerCase() === key.toLowerCase()) {
+					onSelectLetter(key);
+				}
+			});
+		}
+	});
+
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyboard);
+		return () => {
+			document.removeEventListener("keydown", handleKeyboard);
+		};
+	}, [handleKeyboard]);
+
 	// key component takes in the value of the key
 	return (
-		<div className="keyboard">
+		<div className="keyboard" onKeyDown={handleKeyboard}>
 			<div className="line1">
 				{keys1.map((key) => {
 					return <Key keyValue={key} />;
