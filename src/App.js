00a601c53ae7ebb1ed8929196f59743981c245f4
Elizabeth <tjeudy.e@gmail.com>
2023-03-08 12:59:56 -0500

    function for letter positioning of each cell

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ export const AppContext = createContext();
 
 export default function App() {
 	const [board, setBoard] = useState(boardDefault);
+	// keeps track of letter moving to cells with each attempt and letter positions
+	const [currentAttempt, setCurrentAttempt] = useState({
+		attempt: 0,
+		letterPosition: 0,
+	});
 	return (
 		<div className="App">
 			<div className="container">
 				<Nav />
 				{/* gives access to pass the states with value */}
-				<AppContext.Provider value={{ board, setBoard }}>
+				<AppContext.Provider
+					value={{ board, setBoard, currentAttempt, setCurrentAttempt }}
+				>
 					<div className="game">
 						<Board />
 					</div>
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Key({ keyValue, bigKey }) {
+	// gets access from the board (state)
+	const { board, setBoard, currentAttempt, setCurrentAttempt } =
+		useContext(AppContext);
+
+	// function allows you to select letter from keyboard and add to the game board
+	const selectLetter = () => {
+		if (keyValue === "ENTER") {
+			if (currentAttempt.letterPosition !== 5) return;
+			setCurrentAttempt({
+				attempt: currentAttempt.attempt + 1,
+				letterPosition: 0,
+			});
+		} else {
+			if (currentAttempt.letterPosition > 4) return;
+
+			// checks which letter was selected
+			const newBoard = [...board];
+			// changes the current board based on the current attempt with positioning of letter adding one to move each letter forward
+			newBoard[currentAttempt.attempt][currentAttempt.letterPosition] =
+				keyValue;
+			setBoard(newBoard);
+			setCurrentAttempt({
+				...currentAttempt,
+				letterPosition: currentAttempt.letterPosition + 1,
+			});
+		}
+	};
+
+	// id bigKey depends on if a key is big or not
 	return (
-		<div className="key" id={bigKey && "big"}>
+		<div className="key" id={bigKey && "big"} onClick={selectLetter}>
 			{keyValue}
 		</div>
 	);
 }
-// id bigKey depends on if a key is big or not
