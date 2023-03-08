eedd12a1c7832847ab9f8680fede4f7e372a2dcd
Elizabeth <tjeudy.e@gmail.com>
2023-03-08 11:07:11 -0500

    imported key component / added react icon

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,28 +1,32 @@
 import React from "react";
 import "./Keyboard.css";
+import Key from "./Key";
+import { RiDeleteBack2Line } from "react-icons/ri";
 
 export default function Keyboard() {
 	// keys for each line
 	const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 	const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 	const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
-
+	// key component takes in the value of the key
 	return (
 		<div className="keyboard">
 			<div className="line1">
 				{keys1.map((key) => {
-					return <div> {key} </div>;
+					return <Key keyValue={key} />;
 				})}
 			</div>
 			<div className="line2">
 				{keys2.map((key) => {
-					return <div> {key} </div>;
+					return <Key keyValue={key} />;
 				})}
 			</div>
 			<div className="line3">
+				<Key keyValue={"ENTER"} bigKey />
 				{keys3.map((key) => {
-					return <div> {key} </div>;
+					return <Key keyValue={key} />;
 				})}
+				<Key keyValue={<RiDeleteBack2Line />} bigKey />
 			</div>
 		</div>
 	);
