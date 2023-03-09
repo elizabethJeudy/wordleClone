a956546d6059d39d42b8be52f1c13e88e1ce047b
Elizabeth <tjeudy.e@gmail.com>
2023-03-08 20:41:18 -0500

    added function to check if letter is correct

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -2,7 +2,23 @@ import React, { useContext, useState } from "react";
 import { AppContext } from "../App";
 
 export default function Letters({ letterPosition, attemptValue }) {
-	const { board } = useContext(AppContext);
+	const { board, correctWord, currentAttempt } = useContext(AppContext);
 	const letters = board[attemptValue][letterPosition];
-	return <div className="letters">{letters}</div>;
+
+	// checks if correct or not
+	const correct = correctWord[letterPosition] === letters;
+	const almost = !correct && letters !== "" && correctWord.includes(letters);
+	// checks if letter is correct, almost, or wrong
+	// if correct if true then set to correct;
+	// if false set to almost;
+	// if neither are true set to wrong
+	const letterState =
+		currentAttempt.attempt > attemptValue &&
+		(correct ? "correct" : almost ? "almost" : "wrong");
+
+	return (
+		<div className="letters" id={letterState}>
+			{letters}
+		</div>
+	);
 }
