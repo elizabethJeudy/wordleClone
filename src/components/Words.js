e618594fff1e67b6973e73941539982834106208
Elizabeth <tjeudy.e@gmail.com>
2023-04-04 00:37:29 -0400

    created generate word function

--- src/components/Words.js
+++ src/components/Words.js
@@ -1,3 +1,5 @@
+import wordBank from "./word-bank.txt";
+
 // each array represents each attempt at a word
 export const boardDefault = [
 	["", "", "", "", ""],
@@ -8,3 +10,19 @@ export const boardDefault = [
 	["", "", "", "", ""],
 	["", "", "", "", ""],
 ];
+
+// function will read off words in word bank and create a word set
+// the word bank is fetched using async/await
+// the response will then formula response to text
+// grabs final results
+export const generateWordSet = async () => {
+	let wordSet;
+	await fetch(wordBank)
+		.then((response) => response.text())
+		.then((result) => {
+			const wordArr = result.split("\n");
+			wordSet = new Set(wordArr);
+		});
+
+	return { wordSet };
+};
